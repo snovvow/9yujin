8997d7362c4be8c653b0eebbd24d55cd05e85752
한규진 <david0218@naver.com>
2022-03-18 01:58:37 +0900

    [BOJ] 계단오르기  / 실버3

--- 1-DP/[2579]계단 오르기.py
+++ 1-DP/[2579]계단 오르기.py
@@ -1,6 +1,26 @@
+import sys
+
 n = int(input())
 m = []
 for i in range(n):
     m.append(int(input()))
 
-print(m)
+d = [0] * n
+
+d[0] = m[0]
+if n == 1:
+    print(d[0])
+    sys.exit(0)
+
+d[1] = m[1]+m[0]
+if n == 2:
+    print(d[1])
+    sys.exit(0)
+    
+d[2] = max(m[0]+m[2], m[1]+m[2]) 
+
+
+for i in range(3, n):
+    d[i] = max(d[i-2] + m[i], d[i-3] + m[i-1] + m[i])
+
+print(d[n-1])
