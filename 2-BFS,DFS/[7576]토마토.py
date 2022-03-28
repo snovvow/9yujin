9f974b4d3c71d34c759dd14ae206c6169aaa5071
한규진 <david0218@naver.com>
2022-03-28 21:06:54 +0900

    [BOJ] 토마토 / 골드5

---
+++ 2-BFS,DFS/[7576]토마토.py
@@ -0,0 +1,48 @@
+from ipaddress import collapse_addresses
+
+
+#bfs : deque 사용
+from collections import deque
+
+def bfs():
+    while queue:
+        # 익은토마토 하나 큐에 넣어
+        x,y = queue.popleft()
+        
+        # 네가지 방향
+        for i in range(4):
+            # 다음 좌표
+            nx, ny = dx[i] + x, dy[i] + y
+            if -1 < nx < m and -1 < ny < n and graph[nx][ny] == 0:
+                # 안익은 토마토가 있으면 큐에 넣고, +1
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append([nx,ny])
+
+
+n, m = map(int, input().split())
+graph = []
+queue =deque([])
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+
+for i in range(m):
+    graph.append(list(map(int, input().split())))
+
+    # 익은 토마토는 큐에 저장
+    for j in range(n):
+        if graph[i][j] == 1:
+            queue.append([i,j])
+
+bfs()
+
+maxValue = 0
+for i in graph:
+    for j in i:
+        # 익지 않은 토마토가 있을떄
+        if j == 0:
+            print(-1)
+            exit()
+    maxValue = max(maxValue, max(i))
+print(maxValue-1)
